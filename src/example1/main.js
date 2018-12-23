414291573583d65ec4d4a2c7b2a96494a5935bde
Shawn Oakley <shawn.oakley@youvisit.com>
2018-12-22 20:54:35 -0500

    Adds phaser game code

--- src/example1/main.js
+++
@@ -1,4 +0,0 @@
-var game = new Phaser.Game(600, 400, Phaser.AUTO);
-game.state.add('state1', demo.state1);
-game.state.start('state1');
-console.log("CALLED");
--- src/example1/state2.js
+++
@@ -1,8 +0,0 @@
-demo.state2 = function (){};
-demo.state2.prototype = {
-    preload: function(){},
-    create: function(){
-        game.stage.backgroundColor = '#DDDDDD';
-    },
-    update: function(){}
-};
